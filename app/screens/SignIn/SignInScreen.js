64909f8831269f95294474d2c4bd9a01098f1d29
Rodolfo Peixoto <rodolfog.peixoto@gmail.com>
2018-05-01 22:49:05 -0300

    React Navigation install and setup

---
+++ app/screens/SignIn/SignInScreen.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import { View, Text } from 'react-native';
+
+export default class SignInScreen extends Component{
+  render(){
+    return(
+      <View>
+        <Text>Ola mundo.</Text>
+      </View>
+    );
+  }
+}
