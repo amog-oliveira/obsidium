5444aa895deb37d88e96851fb1511826f6e8bfdb
Pedro Silva <pedro.rls93@gmail.com>
2018-05-17 14:15:26 -0300

    app now saves data on firebase

--- app/screens/SignIn/LoginFB.js
+++ app/screens/SignIn/LoginFB.js
@@ -4,6 +4,7 @@ import {
     View,
     Text,
     TouchableOpacity,
+    Alert,
 
 } from 'react-native';
 import Expo from 'expo';
@@ -14,26 +15,49 @@ const id = '1179605262181506';
 
 export default class LoginFB extends Component{
 
-    login = async () => { 
-            const {type, token} =  await Expo.Facebook.logInWithReadPermissionsAsync(id, {permissions: [ 'public_profile', 'email'] })
-            
-            if (type == 'success'){
-                
-                const response = await fetch(
-                `https://graph.facebook.com/me?access_token=${token}&fields=id,name,email,about,picture`);
+    saveUserDataOnFireBase = (userFacebookInfo) => {
+        const userId = firebase.auth().currentUser.uid;
+        const params = {
+            facebookId: userFacebookInfo.id,
+            name: userFacebookInfo.name,
+            email: userFacebookInfo.email,
+            photoUrl: userFacebookInfo.picture.data.url
+        };
 
-                const json = await response.json();
+        firebase.database().ref('profiles/' + userId).set(params)
+        .then((data) => {
+            console.log('Data was saved\n' + data);
+        }).catch((error) => {
+            console.log('Data could not be saved\n' + error);
+        })
+    }
 
-            console.log('USER INFO', json);    
-            
+    firebaseLogin = (credential, userFacebookInfo) => {
+        firebase.auth().signInWithCredential(credential)
+        .then(() => {
+            this.saveUserDataOnFireBase(userFacebookInfo);
+        }).catch((error) => {
+            Alert.alert(error);
+        })
+    }
+
+    facebookLogin = async () => { 
+        const {type, token} =  await Expo.Facebook.logInWithReadPermissionsAsync(id, {permissions: [ 'public_profile', 'email'] })        
+        if (type == 'success'){            
+            const response = await fetch(
+                `https://graph.facebook.com/me?access_token=${token}&fields=id,name,email,about,picture`
+            );
+            const userFacebookInfo = await response.json();
+            const credential = await firebase.auth.FacebookAuthProvider.credential(token);
+            this.firebaseLogin(credential, userFacebookInfo);
         }else{
-            alert(type);
+            Alert.alert("Login", "Login cancelado");
         } 
     }
 
     getButton(){
     return(
-        <TouchableOpacity onPress={() => this.login()}>
+        <TouchableOpacity onPress={() => this.facebookLogin()}>
             <View style={STYLES.buttonStyle}>
                 <Text style={STYLES.textButtonStyle}>
                     Facebook
